96630fa3829c32911b0d228bcdd221636dccf16f
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:15:19 +0200

    added hw 9

---
+++ script.js
@@ -0,0 +1,35 @@
+// zavdannia 1
+// const arr1 = [1,2,3];
+// function arrCopy(){
+//     let arr2 = [5,4,7];
+//     let arr3 = arr2.concat([arr1]);
+//     console.log(arr3);
+// }
+// arrCopy();
+
+// zavdannia 2
+// let arr1 = [4,2,3];
+// function arrToString(){
+//     let some_arr = arr1.join();
+//     console.log(some_arr);
+// }
+// arrToString();
+
+// zavdannia 3
+// let arr1 = ['Ivan','Pavlo','Ira'];
+
+// function some_arr(a){
+//     let arr2 = [];
+//     for(let i = 0; i< a.length;i++){
+//         arr2[i] = arr1[i].length;
+//     }
+//     console.log(arr2);
+// }
+// some_arr(arr1);
+
+// zavdannia 4??????????????????????????????
+// let arr1 = ['html','css','html','js'];
+
+// function removeDuplicates(a){
+    
+// }
